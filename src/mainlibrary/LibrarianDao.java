8a6f37b26f21d45ab1068e19b98f0088c7c00bac
Saraunsh Shewale <47555889+an0ndb9@users.noreply.github.com>
2023-08-14 13:36:37 +0100

    Update LibrarianDao.java
    Patched Code

--- src/mainlibrary/LibrarianDao.java
+++ src/mainlibrary/LibrarianDao.java
@@ -1,17 +1,20 @@
 package mainlibrary;
 
 import java.sql.*;
+import java.security.MessageDigest;
 
 public class LibrarianDao {
 
     public static int save(String name, String password, String email, String address, String city, String contact) {
         int status = 0;
+        
         try {
 
             Connection con = DB.getConnection();
+            String hashedInputPassword = sha256Hash(password);
             PreparedStatement ps = con.prepareStatement("insert into librarian(name,password,email,address,city,contact) values(?,?,?,?,?,?)");
             ps.setString(1, name);
-            ps.setString(2, password);
+            ps.setString(2, hashedInputPassword);
             ps.setString(3, email);
             ps.setString(4, address);
             ps.setString(5, city);
@@ -42,16 +45,28 @@ public class LibrarianDao {
         boolean status = false;
         try {
             Connection con = DB.getConnection();
-            String select = "select * from Librarian where UserName= '" + name + "' and Password='"+ password +"'";
-            Statement selectStatement = con.createStatement();
-            ResultSet rs = selectStatement.executeQuery(select);
-          
+            String hashedInputPassword = sha256Hash(password);
+            PreparedStatement ps=con.prepareStatement("select * from Librarian where UserName=? and Password=?");
+            ps.setString(1,name);
+            ps.setString(2,hashedInputPassword);
+            ResultSet rs = ps.executeQuery();
             status = rs.next();
             con.close();
         } catch (Exception e) {
             System.out.println(e);
         }
-        return status;
+        return status;s
+    }
+    
+        private static String sha256Hash(String input) throws Exception {
+        MessageDigest md = MessageDigest.getInstance("SHA-256");
+        byte[] digest = md.digest(input.getBytes());
+        StringBuilder sb = new StringBuilder();
+
+        for (byte b : digest) {
+            sb.append(String.format("%02x", b & 0xff));
+        }
+        return sb.toString();
     }
 
 }
