30733c20ad8004c44bdd5cf5cc308115a3c6b191
Saraunsh Shewale <47555889+an0ndb9@users.noreply.github.com>
2023-08-14 13:35:31 +0100

    Update DB.java
    Patched Code

--- src/mainlibrary/DB.java
+++ src/mainlibrary/DB.java
@@ -1,14 +1,25 @@
+
+/*
+Create database.properties file and store the following credentials:
+
+user=root
+password=dummyPa@ssw0rd!
+
+*/
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 package mainlibrary;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
+import java.io.FileInputStream;
 
 /**
  *
@@ -16,19 +27,30 @@ import java.util.Properties;
  */
 public class DB {
 
-    public static String user = "root";
     public static String connection = "jdbc:mysql://localhost:3306/library?autoReconnect=true&useSSL=false";
     
     public static Connection getConnection() {
         Connection con = null;
         try {
             Properties props = new Properties();
-            props.put("user", user);
-     //change the password to the password  ↓↓↓↓↓↓↓↓↓↓↓   you enteredwhen setting up mysql
-            props.put("password", "password");
+            FileInputStream in = new FileInputStream("src\\mainlibrary\\database.properties");
+            props.load(in);
+            in.close();
+            
+            /*  Original Code
+                
+                props.put("user", user);
+                //change the password to the password  ↓↓↓↓↓↓↓↓↓↓↓   you enteredwhen setting up mysql
+                props.put("password", "dummyPa@ssw0rd!");
+            
+            */            
+            // Modified Code
+            String user = props.getProperty("user");
+            String password = props.getProperty("password");
+
             props.put("useUnicode", "true");
-            props.put("useServerPrepStmts", "false"); // use client-side prepared statement
-            props.put("characterEncoding", "UTF-8"); // ensure charset is utf8 here
+            props.put("useServerPrepStmts", "false"); 
+            props.put("characterEncoding", "UTF-8"); 
 
             Class.forName("com.mysql.jdbc.Driver");
             con = DriverManager.getConnection(connection, props);
